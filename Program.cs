e728b83ab5bdeaacbd4a64286264b28ee7362054
Ammon Whitaker <AmmWhi48@millardk12.org>
2026-02-06 11:41:34 -0700

    started user queries

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This program takes various inputs from the user to give a table summary of a planned road trip.
+
+Console.Clear();
+
+Console.WriteLine("We are going to map out a road trip for Fall Break.\nI'm going to ask you some specific questions about the trip, and then I'll callculate a trip summary from that information.");
+
+Console.Write("Where will you be driving? ");
+string destination = Console.ReadLine();
+Console.Write("Who will be driving? ");
+string driver = Console.ReadLine();
+
+//TODO: Think through a system to make the types and verification streamlined.
+Console.Write("Who will be driving? ");
+string distance = Console.ReadLine();
